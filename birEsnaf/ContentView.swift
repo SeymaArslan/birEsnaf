d6675ccd72d283179ef4d0e24763370b3ee746c0
SeymaArslan <seymarslannn@gmail.com>
2024-12-12 21:50:48 +0300

    Initial Commit

---
+++ birEsnaf/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  birEsnaf
+//
+//  Created by Seyma Arslan on 12.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ birEsnaf/birEsnafApp.swift
@@ -0,0 +1,17 @@
+//
+//  birEsnafApp.swift
+//  birEsnaf
+//
+//  Created by Seyma Arslan on 12.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct birEsnafApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
